1646669d00e814ce99a84d5bdeaaafd2fcfa29fd
david <david.jian@myre.life>
2025-09-19 11:59:39 +0800

    feat: integrate chrome-aws-lambda for Puppeteer in Vercel environment
    - Added chrome-aws-lambda dependency to package.json.
    - Updated puppeteer utility to use puppeteer-core and chrome-aws-lambda.
    - Implemented conditional logic to determine executable path and headless mode based on Vercel environment.

--- src/utilities/puppeteer.util.ts
+++ src/utilities/puppeteer.util.ts
@@ -1,6 +1,10 @@
-import puppeteer, { type Browser } from 'puppeteer';
+import core, { type Browser } from 'puppeteer-core';
+import chrome from 'chrome-aws-lambda';
 
 const IDLE_TIME = 1000 * 20;
+
+// 判斷是否在 Vercel 環境
+const isVercel = !!process.env.VERCEL;
 
 const createPuppeteerUtil = () => {
   let browser: Browser;
@@ -9,9 +13,11 @@ const createPuppeteerUtil = () => {
     if (browser.connected) {
       return browser;
     }
-    browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+    browser = await core.launch({
+    args: chrome.args,
+    executablePath: isVercel ? await chrome.executablePath : '/path/to/your/local/chrome', // 本地開發時指向你的 Chrome
+    headless: isVercel ? chrome.headless : true,
+  });
 
     return browser;
   }
